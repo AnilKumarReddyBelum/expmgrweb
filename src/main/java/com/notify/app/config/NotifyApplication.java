ee7b73309e0720de68e7d352ceba2447070b3f37
anilkumarreddy <AnilKumarReddyBelum@users.noreply.github.com>
2017-04-28 14:41:04 +0530

    NotifyApplication class has been updated with the local profile
    NotifyApplication class has been updated with the local profile

--- src/main/java/com/notify/app/config/NotifyApplication.java
+++ src/main/java/com/notify/app/config/NotifyApplication.java
@@ -16,6 +16,19 @@ import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.notify.app.config", "com.notify.app.controller", "com.notify.app.model",
@@ -45,5 +58,12 @@ public class NotifyApplication extends SpringBootServletInitializer {
 		SimpleCacheManager cacheManager = new SimpleCacheManager();
 		cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("expmgrwebcache")));
 		return cacheManager;
+	}
+	
+	@Profile("local")
+	@Bean(destroyMethod = "shutdown")
+	public EmbeddedDatabase dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.build();
 	}
 }

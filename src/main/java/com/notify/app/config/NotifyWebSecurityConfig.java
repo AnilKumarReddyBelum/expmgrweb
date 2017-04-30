68b68ec159270bf17b363fd77effda2c457465cd
anilkumarreddy <AnilKumarReddyBelum@users.noreply.github.com>
2017-04-30 22:55:55 +0530

    Update Security Config Info
    Update Security Config Info

--- src/main/java/com/notify/app/config/NotifyWebSecurityConfig.java
+++ src/main/java/com/notify/app/config/NotifyWebSecurityConfig.java
@@ -24,12 +24,10 @@ public class NotifyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//http.authorizeRequests().antMatchers("/resources/**", "/registration").permitAll().anyRequest().authenticated()
-		//		.and().formLogin().loginPage("/login").permitAll().and().logout().permitAll().and().csrf().disable();
-				//.anyRequest().hasRole("ADMIN").and().csrf().disable();
-		
-		http.authorizeRequests().antMatchers("/resources/**", "/registration","/customer/**","/menu/**").permitAll().anyRequest().authenticated()
-		.and().formLogin().loginPage("/login").permitAll().and().csrf().disable();
+		http.authorizeRequests().antMatchers("/resources/**", "/registration", "/customer/**", "/menu/**").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
+
 	}
 
 	@Autowired

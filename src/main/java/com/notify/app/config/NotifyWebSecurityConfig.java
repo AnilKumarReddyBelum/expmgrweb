9938afb53f362d18afb7e292c5a4d68d11cd9c31
anilkumarreddy <AnilKumarReddyBelum@users.noreply.github.com>
2017-05-01 15:34:28 +0530

    disable csrf nature
    disable csrf nature

--- src/main/java/com/notify/app/config/NotifyWebSecurityConfig.java
+++ src/main/java/com/notify/app/config/NotifyWebSecurityConfig.java
@@ -26,7 +26,7 @@ public class NotifyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/resources/**", "/registration", "/customer/**", "/menu/**").permitAll()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.permitAll();
+				.permitAll().and().csrf().disable();
 
 	}
 

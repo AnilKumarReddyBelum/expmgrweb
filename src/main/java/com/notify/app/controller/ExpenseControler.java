cd5147c1b4b9efad544aff1612a58e003715e1ca
anilkumarreddy <AnilKumarReddyBelum@users.noreply.github.com>
2017-09-22 09:37:40 +0530

    Added sendSMSoverTwilio api to the ExpenseControler class
    Added sendSMSoverTwilio api to the ExpenseControler class

--- src/main/java/com/notify/app/controller/ExpenseControler.java
+++ src/main/java/com/notify/app/controller/ExpenseControler.java
@@ -15,13 +15,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.notify.app.model.Expense;
 
 
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.SmsFactory;
+import com.twilio.sdk.resource.instance.Account;
+
 @RestController
 public class ExpenseControler {
 
 	private static Logger EXPENSE_CONTROLLER_LOGGER = Logger.getLogger(ExpenseControler.class);
+	
+	
+	
 
 	@Autowired
 	private com.notify.app.repo.ExpenseRepository expenseRepository;
+	
+	
+	public static final String ACCOUNT_SID = "AC766b60408feb07e32867e683fbb600e9";
+	public static final String AUTH_TOKEN = "fd8667ebac199f98c9948e2aff971f72";
+
+	@RequestMapping("/sendSMSoverTwilio")
+	public String sendSMSoverTwilio() {
+		String messageSentStaus = null;
+		TwilioRestClient twilioRestClient = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+		Account account = twilioRestClient.getAccount();
+		SmsFactory factory = account.getSmsFactory();
+		Map<String, String> messageMap = new HashMap<String, String>();
+		messageMap.put("To", "+918884631730");
+		messageMap.put("From", "+919502106030");
+		messageMap.put("Body", "Hello! This is Anil from Twilio & Thanks Twilio!");
+		try {
+			factory.create(messageMap);
+			messageSentStaus = "Success!";
+		} catch (TwilioRestException e) {
+			messageSentStaus = e.getErrorMessage();
+		}
+
+		return messageSentStaus;
+
+	}
 
 	
 	@RequestMapping("/helloCache/{name}")
